ed4ae9695e68fdef27f620a1f39d539bfa718483
Ujjwal Shivacharya <81429137+ujjwalshiva@users.noreply.github.com>
2025-02-10 14:24:26 +0530

    Update main.py

--- firmware/main.py
+++ firmware/main.py
@@ -1,7 +1,17 @@
 import machine
 import network
+import time
 
 led = machine.Pin(2, machine.Pin.OUT)
 wlan = network.WLAN(network.STA_IF)
 
 print(wlan.ifconfig())
+
+def blink_led():
+    while True:
+        led.on()
+        time.sleep(1)
+        led.off()
+        time.sleep(1)
+
+blink_led()
